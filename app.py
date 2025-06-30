b1df899fe213a8f8e4a1f1cf80898b784b7c9c6e
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2025-06-30 21:12:14 +0545

    add expense funct complete

--- app.py
+++ app.py
@@ -1,51 +1,54 @@
-import argparse,os,json
+import argparse
+import os
+import json
 import datetime
 
 EXPENSE_FILE = "expense.json"
 
-
 def load_expense():
     if not os.path.exists(EXPENSE_FILE):
         return []
-    with open(EXPENSE_FILE,"r") as f:
+    with open(EXPENSE_FILE, "r") as f:
         return json.load(f)
 
-def save_expense(expense):
-    with open(EXPENSE_FILE,"w") as f:
-        json.dump(expense, f, indent=4)
+def save_expense(expenses):
+    with open(EXPENSE_FILE, "w") as f:
+        json.dump(expenses, f, indent=4)
 
-
-def add_expense(description,amount):
+def add_expense(description, amount):
     expenses = load_expense()
-    new_id = 1 if not expenses else max(expense["id"] for expense in expenses) + 1
+    new_id = 1 if not expenses else max(exp["id"] for exp in expenses) + 1
     new_expense = {
-        "id":new_id,
-        "description":description,
-        "amount":amount,
-        "date":datetime.datetime.now()
+        "id": new_id,
+        "description": description,
+        "amount": amount,
+        "date": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
     }
     expenses.append(new_expense)
     save_expense(expenses)
-    print(f"Expense added successfully (ID: {id})")
+    print(f"Expense added successfully (ID: {new_id})") 
 
-def main():
+def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog='ExpenseTrackerCLI',
         description='Track your expenses easily.',
         epilog='Built By UtsavAdhikari3'
     )
-    sub = parser.add_subparsers(dest="command", required=False)
+    sub = parser.add_subparsers(dest="command", required=True)
 
-    # add Subcommands
-    add = sub.add_parser("add", help="Add an expense with --description and amount")
-    add.add_argument("--description", type=str,required=True)
-    add.add_argument("--ammount", type=int, required=True)
+    # Add subcommand
+    add = sub.add_parser("add", help="Add an expense")
+    add.add_argument("--description", type=str, required=True, help="Description of the expense")
+    add.add_argument("--amount", type=int, required=True, help="Amount spent")  
 
+    return parser  
 
+def main():
+    parser = build_parser()
+    args = parser.parse_args()
 
-
-
-
+    if args.command == "add":
+        add_expense(args.description, args.amount)
 
 if __name__ == "__main__":
     main()
